95f778b28c8722aa9e241de7e226ed7c8f1d12d7
darlwhizkid <darlingtonodom@gmail.com>
2025-01-21 11:09:52 +0100

    Updated API endpoint for reviews system

--- assets/js/reviews.js
+++ assets/js/reviews.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000';
+const API_URL = "https://soma-backend-2syz.onrender.com";
 
 // Handle star rating selection
 const stars = document.querySelectorAll('.star-rating i');
